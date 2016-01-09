c7a855c3a760a573344f6d23426cd1a9242ac181
haerelina <hroy345@gmail.com>
2016-01-10 01:30:28 +0530

    Form without ModelForm

--- src_pycommerce/register/views.py
+++ src_pycommerce/register/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import SignUpForm
+from .forms import ContactForm, SignUpForm
 # Create your views here.
 def home(request):
 	title = 'Welcome'
@@ -20,4 +20,23 @@ def home(request):
 	    "title": "Thank you"
 	}
 	return render(request, "home.html", context)
+
+
+def contact(request):
+	form = ContactForm(request.POST or None)
+	if form.is_valid():
+		for key in form.cleaned_data:
+			print key
+			print form.cleaned_data.get(key)
+		email = form.cleaned_data.get("email")
+		message = form.cleaned_data.get("message")
+		full_name = form.cleaned_data.get("full_name")
+		print email, message, full_name
+
+
+	context = {
+	    "form": form,
+
+	}
+	return render(request, "forms.html", context)
 	
