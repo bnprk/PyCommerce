4dd09b26c8c816e684ad5001eba2b61ad7fdba73
haerelina <hroy345@gmail.com>
2016-01-11 21:20:52 +0530

    Serving static files

--- src_pycommerce/src/urls.py
+++ src_pycommerce/src/urls.py
@@ -1,10 +1,20 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     # Examples:
     url(r'^$', 'register.views.home', name='home'),
+    url(r'^contact/$', 'register.views.contact', name='contact'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	
+
